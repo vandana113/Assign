61755adc39f48ceb691da139aac5bf74f3ad2265
Vandana Kumari <vandanakumari13998@gmail.com>
2020-06-02 01:09:32 +0530

    commit with push to structure of project

---
+++ flipkart-demo/src/com/flipkart/demo/DemoProject.java
@@ -0,0 +1,11 @@
+package com.flipkart.demo;
+
+public class DemoProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
